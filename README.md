e72e8efe50ebee48056db1f283764de40a3f8863
David Yell <dyell@ukwebmedia.com>
2014-10-30 11:57:03 +0000

    First draft with composer support

---
+++ README.md
@@ -0,0 +1,7 @@
+#NumbersToWordsHelper
+A helper for CakePHP3 to convert numbers into words.
+
+Have you ever wanted to use a number as a css class? Or title items in a loop, but number just look messy? I have, and so I made this quick plugin to solve that problem.
+
+##Installation
+Simply copy the helper
---
+++ View/Helper/NumbersToWordsHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *  * NumbersToWordsHelper
+ *   * Convert numbers to words with an without an ordinal suffix
+ *    *
+ *     * User: David Yell <neon1024@gmail.com>
+ *      * Date: 30/10/14
+ *       * Time: 10:47
+ *        */
+
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+use NumberFormatter;
+
+class NumbersToWordsHelper extends Helper {
+
+	protected $_defaultConfig = [
+		'locale' => 'en_GB'
+	];
+
+	public function __construct(View $view, array $config = []) {
+		parent::__construct($view, $config);
+	}
+
+/**
+ *  * Convert a number into a word
+ *   *
+ *    * @param $number An integer number to convert
+ *     * @return string
+ *      */
+	public function spell($number) {
+		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::SPELLOUT);
+		return $formatter->format($number);
+	}
+
+/**
+ *  * Convert a number into it's ordinal type
+ *   * eg, 1 -> 1st, 2 -> 2nd
+ *    *
+ *     * @param $number An integer number to convert
+ *      * @return string
+ *       */
+	public function ordinal($number) {
+		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::ORDINAL);
+		return $formatter->format($number);
+	}
+
+} 
