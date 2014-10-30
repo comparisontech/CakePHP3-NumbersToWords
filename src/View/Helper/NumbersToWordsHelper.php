b01b87717bfbe36dca72f07417c6707cde78b457
David Yell <dyell@ukwebmedia.com>
2014-10-30 12:27:09 +0000

    Fixed the namespace and the comments

--- src/View/Helper/NumbersToWordsHelper.php
+++ src/View/Helper/NumbersToWordsHelper.php
@@ -1,14 +1,14 @@
 <?php
 /**
- *  * NumbersToWordsHelper
- *   * Convert numbers to words with an without an ordinal suffix
- *    *
- *     * User: David Yell <neon1024@gmail.com>
- *      * Date: 30/10/14
- *       * Time: 10:47
- *        */
+ * NumbersToWordsHelper
+ * Convert numbers to words with an without an ordinal suffix
+ *
+ * User: David Yell <neon1024@gmail.com>
+ * Date: 30/10/14
+ * Time: 10:47
+*/
 
-namespace App\View\Helper;
+namespace NumbersToWords\View\Helper;
 
 use Cake\View\Helper;
 use Cake\View\View;
@@ -25,26 +25,25 @@ class NumbersToWordsHelper extends Helper {
 	}
 
 /**
- *  * Convert a number into a word
- *   *
- *    * @param $number An integer number to convert
- *     * @return string
- *      */
+ * Convert a number into a word
+ *
+ * @param $number An integer number to convert
+ * @return string
+ */
 	public function spell($number) {
 		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::SPELLOUT);
 		return $formatter->format($number);
 	}
 
 /**
- *  * Convert a number into it's ordinal type
- *   * eg, 1 -> 1st, 2 -> 2nd
- *    *
- *     * @param $number An integer number to convert
- *      * @return string
- *       */
+ * Convert a number into it's ordinal type
+ * eg, 1 -> 1st, 2 -> 2nd
+ *
+ * @param $number An integer number to convert
+ * @return string
+*/
 	public function ordinal($number) {
 		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::ORDINAL);
 		return $formatter->format($number);
 	}
-
 } 
