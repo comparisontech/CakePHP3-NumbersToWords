bf205995a91386abbae58fbb3058ed605bd8f9b2
David Yell <dyell@ukwebmedia.com>
2016-10-10 10:40:56 +0100

    Refactored the helper so that it can convert numbers which are within strings. Also added some tests

--- src/View/Helper/NumbersToWordsHelper.php
+++ src/View/Helper/NumbersToWordsHelper.php
@@ -6,44 +6,62 @@
  * User: David Yell <neon1024@gmail.com>
  * Date: 30/10/14
  * Time: 10:47
-*/
+ */
 
 namespace NumbersToWords\View\Helper;
 
 use Cake\View\Helper;
-use Cake\View\View;
 use NumberFormatter;
 
-class NumbersToWordsHelper extends Helper {
+class NumbersToWordsHelper extends Helper
+{
 
-	protected $_defaultConfig = [
-		'locale' => 'en_GB'
-	];
+    /**
+     * Default locale
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'locale' => 'en_GB'
+    ];
 
-	public function __construct(View $view, array $config = []) {
-		parent::__construct($view, $config);
-	}
+    /**
+     * Convert a number into a word
+     *
+     * @param int|string $item An integer number to convert or a string containing an integer
+     * @return string
+     */
+    public function spell($item)
+    {
+        $formatter = new NumberFormatter($this->config['locale'], NumberFormatter::SPELLOUT);
 
-/**
- * Convert a number into a word
- *
- * @param $number An integer number to convert
- * @return string
- */
-	public function spell($number) {
-		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::SPELLOUT);
-		return $formatter->format($number);
-	}
+        if (is_int($item)) {
+            return $formatter->format($item);
+        }
 
-/**
- * Convert a number into it's ordinal type
- * eg, 1 -> 1st, 2 -> 2nd
- *
- * @param $number An integer number to convert
- * @return string
-*/
-	public function ordinal($number) {
-		$formatter = new NumberFormatter($this->config['locale'], NumberFormatter::ORDINAL);
-		return $formatter->format($number);
-	}
-} 
+        return preg_replace_callback('/([0-9]+)/', function ($matches) use ($formatter) {
+            return $formatter->format($matches[0]);
+        }, $item);
+    }
+
+    /**
+     * Convert a number into it's ordinal type
+     * eg, 1 -> 1st, 2 -> 2nd
+     *
+     * @param int|string $item An integer number to convert or a string containing a number
+     *
+     * @return string
+     */
+    public function ordinal($item)
+    {
+        $formatter = new NumberFormatter($this->config['locale'], NumberFormatter::ORDINAL);
+
+        if (is_int($item)) {
+            return $formatter->format($item);
+        }
+
+        return preg_replace_callback('/([0-9]+)/', function ($matches) use ($formatter) {
+            return $formatter->format($matches[0]);
+        }, $item);
+    }
+}
---
+++ tests/TestCase/View/Helper/NumbersToWordsHelperTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * NumbersToWordsHelperTest.php
+ *
+ * @author David Yell <dyell@ukwebmedia.com>
+ * @copyright 2016 UK Web Media Ltd
+ */
+
+namespace NumbersToWords\Tests;
+
+use Cake\TestSuite\TestCase;
+use NumbersToWords\View\Helper\NumbersToWordsHelper;
+
+class NumbersToWordsHelperTest extends TestCase
+{
+    /**
+     * Setup before tests
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $View = $this->getMockBuilder('\Cake\View\View')
+            ->getMock();
+
+        $this->NumbersToWords = new NumbersToWordsHelper($View);
+    }
+
+    /**
+     * Teardown after tests
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Numbers and expected output
+     *
+     * @return array
+     */
+    public function numbersProvider()
+    {
+        return [
+            [
+                '2',
+                'two'
+            ],
+            [
+                '99',
+                'ninety-nine'
+            ],
+            [
+                '12345',
+                'twelve thousand three hundred forty-five'
+            ],
+            [
+                'I found 3 apples',
+                'I found three apples'
+            ],
+            [
+                '99 problems, but tests aint 1',
+                'ninety-nine problems, but tests aint one'
+            ]
+        ];
+    }
+
+    /**
+     * Spell numbers
+     *
+     * @dataProvider numbersProvider
+     */
+    public function testSpellingNumbers($number, $expected)
+    {
+        $result = $this->NumbersToWords->spell($number);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function ordinalsProvider()
+    {
+        return [
+            [
+                '2',
+                '2nd'
+            ],
+            [
+                '5',
+                '5th'
+            ],
+            [
+                '123',
+                '123rd'
+            ],
+            [
+                'I dont want to come 2',
+                'I dont want to come 2nd'
+            ],
+            [
+                'Set it for July 4',
+                'Set it for July 4th'
+            ]
+        ];
+    }
+
+    /**
+     * Ordinals
+     *
+     * @dataProvider ordinalsProvider
+     */
+    public function testOrdinals()
+    {
+        $result = $this->NumbersToWords->ordinal(5);
+        $this->assertEquals('5th', $result);
+    }
+
+}
